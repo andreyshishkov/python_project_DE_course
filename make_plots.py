dfed06a7623f80146da8b82625940d26ceca953d
Andrey Shishkov <shishkoffandrey@yandex.ru>
2024-08-19 12:36:36 +0300

    Add make_sale_date_plot function

--- make_plots.py
+++ make_plots.py
@@ -1,4 +1,5 @@
 import seaborn as sns
+from datetime import date
 import matplotlib.pyplot as plt
 
 
@@ -10,3 +11,13 @@ def make_sale_product_plot(sale_product_data: dict[str, int]) -> None:
     plt.title('Распределение выручки по продуктам')
 
     plt.savefig('sale-product-distribution.png')
+
+
+def make_sale_date_plot(sale_date_data: dict[date, int]) -> None:
+    plt.figure(figsize=(15, 10))
+    sns.barplot(sale_date_data)
+    plt.xlabel('Дата')
+    plt.ylabel('Суммарная выручка')
+    plt.title('Распределение выручки по датам')
+
+    plt.savefig('sale-date-distribution.png')
